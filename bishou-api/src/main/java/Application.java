1450333c2f7d5e431622b39da0d32a75bd97e210
liyongchao <liyongchao@corp.rrzcp8.com>
2018-08-31 16:59:07 +0800

    增加log日志打印

--- bishou-api/src/main/java/Application.java
+++ bishou-api/src/main/java/Application.java
@@ -10,7 +10,7 @@ public class Application
 
 	public static void main(String[] args)
 	{
-//		System.setProperty("druid.logType", "log4j2");
+		System.setProperty("druid.logType", "log4j2");
 		//设置配置文件
 //		System.setProperty("spring.config.location", "classpath:config.properties");
 		SpringApplication.run(Application.class, args);
--- bishou-api/src/main/java/com/lkzlee/bishou/IndexController.java
+++ bishou-api/src/main/java/com/lkzlee/bishou/IndexController.java
@@ -1,5 +1,6 @@
 package com.lkzlee.bishou;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2018/8/31 15:27
  * @Desc: 首页调试controller
  */
+@Log4j2
 @RestController
 public class IndexController
 {
 	@RequestMapping("/index")
-	public String index()
+	public String index(String p)
 	{
+		log.info("--start request index,param p={}", p);
 		return "Bi Shou Hello";
 	}
 }
